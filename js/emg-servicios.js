455c19f089f6c82c6988694d90a132100c4552f2
Hiram E. Pérez <ctin@MacBook-Pro-de-CTIN.local>
2013-10-07 14:41:31 -0600

    Edicion de emg-servicios
    Se agrega la pagina de servicios

---
+++ js/emg-servicios.js
@@ -0,0 +1,5 @@
+// cuando la secciòn de servicios se carga...
+$('#emg-servicios').on('pageshow',function(e,data){    
+  //$('#index-content').css('margin-top',($(window).height() - $('[data-role=header]').height() - $('[data-role=footer]').height() - $('#index-content').outerHeight())/2);
+  alert('algo');
+});
