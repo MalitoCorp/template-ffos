74121117b46832b5c1a6a7191b57aefbcb2e9cd2
Hiram E. Pérez <ctin@customer-187-174-213-84.uninet-ide.com.mx>
2013-10-08 12:54:33 -0600

    Sin importancia
    sadfjoasdjfoia

--- js/emg-servicios.js
+++ js/emg-servicios.js
@@ -1,5 +1,11 @@
 // cuando la secciòn de servicios se carga...
-$('#emg-servicios').on('pageshow',function(e,data){    
-  $('#index-content').css('margin-top',($(window).height() - $('[data-role=header]').height() - $('[data-role=footer]').height() - $('#index-content').outerHeight())/2);
-  alert('algo');
+$('#emg-servicios').on('pageshow',function(e,data){
+
+  $('#servicios-content').css('margin-top',
+  	                      ($(window).height() 
+  	                      	- $('[data-role=header]').height() 
+  	                      	- $('[data-role=footer]').height() 
+  	                      	- $('#servicios-content').outerHeight())/2);
+  return;
 });
+
